eacf41c8c59db6392bd717917ebedafe96f02d8f
alan <alan_henschel@hotmail.com>
2022-02-20 00:28:56 -0300

    create superuser and linter errors

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
-from enum import unique
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -14,6 +14,14 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model tha uses email instead of username"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,8 @@
-import email
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(TestCase):
 
+class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating new user with an email"""
         email = 'test@recipepython.com'
@@ -29,3 +28,12 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'teste@testepython.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
